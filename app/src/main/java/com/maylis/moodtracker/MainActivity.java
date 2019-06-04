d350f156b36f5d0518072518c72c87f3608e049d
Maylis <maylhedge@hotmail.fr>
2019-06-04 16:19:22 +0200

    no modification Main

--- app/src/main/java/com/maylis/moodtracker/MainActivity.java
+++ app/src/main/java/com/maylis/moodtracker/MainActivity.java
@@ -119,7 +119,6 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
                 Intent HistoryMoodActivity = new Intent(MainActivity.this, HistoryMoodActivity.class);
                 startActivity(HistoryMoodActivity);
 
-
             }
         });
 
