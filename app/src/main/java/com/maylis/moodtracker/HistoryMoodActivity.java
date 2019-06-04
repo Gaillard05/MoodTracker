7a06a0da5333aae5980c57a8c88c3d267ab83788
Maylis <maylhedge@hotmail.fr>
2019-06-04 17:18:14 +0200

    modification History implement load

--- app/src/main/java/com/maylis/moodtracker/HistoryMoodActivity.java
+++ app/src/main/java/com/maylis/moodtracker/HistoryMoodActivity.java
@@ -53,9 +53,10 @@ public class HistoryMoodActivity extends AppCompatActivity {
         mYesterdays = (TextView) findViewById(R.id.activity_history_mood_yesterday_txt);
         mButtonDialogYesterday = (Button) findViewById(R.id.activity_history_mood_yesterday_dialog_btn);
 
-        Storage.load(this,"mood");
+        Storage.load(this, "mood");
     }
 }
+
 
 
 
