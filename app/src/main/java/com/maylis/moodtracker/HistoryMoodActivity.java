83bf7f0a4436493d2cd3a7f002fb2fd9f168db23
Maylis <maylhedge@hotmail.fr>
2019-06-11 14:52:01 +0200

    modification of color background

--- app/src/main/java/com/maylis/moodtracker/HistoryMoodActivity.java
+++ app/src/main/java/com/maylis/moodtracker/HistoryMoodActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
+
 public class HistoryMoodActivity extends AppCompatActivity {
 
     private TextView mWeek;
@@ -60,6 +61,9 @@ public class HistoryMoodActivity extends AppCompatActivity {
         mButtonDialogYesterday.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+
 
             }
         });
