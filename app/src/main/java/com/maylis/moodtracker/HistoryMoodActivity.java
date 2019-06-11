9d9a2866c0c10702ff100545a2cf4bfc7f3bda45
Maylis <maylhedge@hotmail.fr>
2019-06-11 18:04:39 +0200

    modification of layout activity_history

--- app/src/main/java/com/maylis/moodtracker/HistoryMoodActivity.java
+++ app/src/main/java/com/maylis/moodtracker/HistoryMoodActivity.java
@@ -60,7 +60,6 @@ public class HistoryMoodActivity extends AppCompatActivity {
 
         mYesterdays = (TextView) findViewById(R.id.activity_history_mood_yesterday_txt);
         mButtonDialogYesterday = (Button) findViewById(R.id.activity_history_mood_yesterday_dialog_btn);
-
         mYesterdays.setBackgroundResource(R.color.banana_yellow);
 
         Mood r = Storage.load(this,"mood");
