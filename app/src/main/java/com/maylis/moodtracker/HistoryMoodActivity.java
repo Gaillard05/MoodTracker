6315aafb03937b9f04abb9a73b0d600c7241581b
Maylis <maylhedge@hotmail.fr>
2019-06-11 17:58:48 +0200

    modification of layout activity_history

--- app/src/main/java/com/maylis/moodtracker/HistoryMoodActivity.java
+++ app/src/main/java/com/maylis/moodtracker/HistoryMoodActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +25,7 @@ public class HistoryMoodActivity extends AppCompatActivity {
     private TextView mThreeDays;
     private TextView mBeforeYesterdays;
     private TextView mYesterdays;
+    private RelativeLayout mColorYesterday;
 
     private Button mButtonDialogWeek;
     private Button mButtonDialogSixDays;
@@ -41,18 +43,27 @@ public class HistoryMoodActivity extends AppCompatActivity {
 
         mWeek = (TextView) findViewById(R.id.activity_history_mood_week_txt);
         mButtonDialogWeek = (Button) findViewById(R.id.activity_history_mood_week_dialog_btn);
+
         mSixDays = (TextView) findViewById(R.id.activity_history_mood_6days_txt);
         mButtonDialogSixDays = (Button) findViewById(R.id.activity_history_mood_6days_dialog_btn);
+
         mFiveDays = (TextView) findViewById(R.id.activity_history_mood_5days_txt);
         mButtonDialogFiveDays = (Button) findViewById(R.id.activity_history_mood_5days_dialog_btn);
+
         mFourDays = (TextView) findViewById(R.id.activity_history_mood_4days_txt);
         mButtonDialogFourDays = (Button) findViewById(R.id.activity_history_mood_4days_dialog_btn);
+
         mThreeDays = (TextView) findViewById(R.id.activity_history_mood_3days_txt);
         mButtonDialogThreeDays = (Button) findViewById(R.id.activity_history_mood_3days_dialog_btn);
+
         mBeforeYesterdays = (TextView) findViewById(R.id.activity_history_mood_before_yesterday_txt);
         mButtonDialogBeforeYesterday = (Button) findViewById(R.id.activity_history_mood_before_yesterday_dialog_btn);
+
         mYesterdays = (TextView) findViewById(R.id.activity_history_mood_yesterday_txt);
+        mColorYesterday =(RelativeLayout) findViewById(R.id.layout_1_days);
         mButtonDialogYesterday = (Button) findViewById(R.id.activity_history_mood_yesterday_dialog_btn);
+
+        mColorYesterday.setBackgroundResource(R.color.banana_yellow);
 
         Mood r = Storage.load(this,"mood");
         Log.d("HistoryMood","Mood value:"+ r.getMoodIndex());
