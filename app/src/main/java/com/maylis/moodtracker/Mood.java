b0971028859e2f2720133371d2d31becb78e8355
Maylis <maylhedge@hotmail.fr>
2019-06-17 17:32:49 +0200

    Try implement date of calendar method in Mood class

--- app/src/main/java/com/maylis/moodtracker/Mood.java
+++ app/src/main/java/com/maylis/moodtracker/Mood.java
@@ -1,9 +1,13 @@
 package com.maylis.moodtracker;
 
+//import java.text.SimpleDateFormat;
+//import java.util.Calendar;
+//import java.util.Date;
 
 public class Mood {
     private int moodIndex;
     private String dialogCommentView;
+    private String dateInString;
 
 
     public int getMoodIndex() {
@@ -12,9 +16,19 @@ public class Mood {
 
     public void setMoodIndex(int index) {
         this.moodIndex = index;
+
+        //SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        //String dateInString = "17-06-2019 17:20:56";
+        //Date date = sdf.parse(dateInString);
+
+        //Calendar calendar = Calendar.getInstance();
+        //calendar.setTime(date);
     }
 
-    public String getComment() { return dialogCommentView; }
+    public String getComment() {
+        return dialogCommentView;
+    }
+
 
     public void setComment(String comment) { this.dialogCommentView = comment; }
 
