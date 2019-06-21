59b0b35a369b870cd30ad5ed46927cc55cfcf10a
Maylis <maylhedge@hotmail.fr>
2019-06-21 14:58:45 +0200

    Comment method date in class ManageDate

--- app/src/main/java/com/maylis/moodtracker/ManageDateMood.java
+++ app/src/main/java/com/maylis/moodtracker/ManageDateMood.java
@@ -1,31 +1,34 @@
 package com.maylis.moodtracker;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+//import java.text.SimpleDateFormat;
+//import java.util.Calendar;
+//import java.util.Date;
+//import java.util.GregorianCalendar;
+
 
 /**
  * Created by Maylis Gaillard on 18/06/2019.
  */
-public class ManageDateMood {
+public class ManageDateMood{
 
+    private String convertDate;
     private String date;
 
-    public void convertDateToString() {
 
-        // create Calendar date
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMM dd");
 
-        Calendar calendar = new GregorianCalendar(2019, 6, 18);
-        System.out.println("#6. " + sdf.format(calendar.getTime()));
+    //public static void ManageDateMood.convertDate(){
 
-        calendar.set(Calendar.YEAR, 2019);
-        calendar.set(Calendar.MONTH, 6);
-        calendar.set(Calendar.DAY_OF_MONTH, 18);
+        //create calendar date
+        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMM dd");
+        //Calendar calendar = new GregorianCalendar(2019, 6, 18);
+        //System.out.println("#6. " + sdf.format(calendar.getTime()));
 
-        // Convert Date in String
-        String date = sdf.format(new Date());
-        System.out.println(date); //18/06/2019
+        //calendar.set(Calendar.YEAR, 2019);
+        //calendar.set(Calendar.MONTH, 6);
+        //calendar.set(Calendar.DAY_OF_MONTH, 18);
+        //Convert Date in String
+        //String date = sdf.format(new Date());
+        //System.out.println(date); //18/06/2019
+    // return String date;
     }
-}
+//}
