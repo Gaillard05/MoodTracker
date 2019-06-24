778ad39e048e524127d7f573749c9b94a19a55bb
Maylis <maylhedge@hotmail.fr>
2019-06-24 14:52:51 +0200

    try recover value string date in class Mood and convert  date in History

--- app/src/main/java/com/maylis/moodtracker/HistoryMoodActivity.java
+++ app/src/main/java/com/maylis/moodtracker/HistoryMoodActivity.java
@@ -95,7 +95,7 @@ public class HistoryMoodActivity extends AppCompatActivity {
         final Mood r = Storage.load(this, "mood");
         Log.d("HistoryMood", "Mood value:" + r.getMoodIndex());
         Log.d("HistoryMood", "Comment value:" + r.getComment());
-        Log.d("HistoryMood", "date" + r.getDate());
+        Log.d("HistoryMood", "date:" + r.getDate());
 
 
         String day = "18-06-2019";
